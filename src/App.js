74f94f1a74f50a0d28f095c0078bb20ea0c8856f
Matthew Yee <matthewyee@Matthews-MacBook-Pro.local>
2019-07-27 11:47:06 -0700

    testing by hardcode

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/**
+* @author Matthew Yee
+**/
+
+const App = () => {
+  const [monsters, setMonsters] = useState([
+    {
+      name: 'Frankenstein',
+      id: 'a1'
+    },
+    {
+      name: 'Dracula',
+      id: 'a2'
+    },
+    {
+      name: 'Zombie',
+      id: 'a3'
+    }
+  ])
+
+ 
+ return (
+    <div>
+    {
+      monsters.map(monster => <h1 key={monster.id}> { monster.name } </h1>)
+    }
+   </div>
+ )
 }
 
-export default App;
+export default App
